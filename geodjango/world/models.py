20c2e7c93cdf5cd9eb67a000f902fb0b612d24a6
Daniël Geerts <daniel.geerts@live.nl>
2021-04-30 02:00:12 +0200

    Fixed migrations. Installation is complete.

--- geodjango/world/models.py
+++ geodjango/world/models.py
@@ -3,6 +3,7 @@ from django.contrib.gis.db import models
 class WorldBorder(models.Model):
     # Regular Django fields corresponding to the attributes in the
     # world borders shapefile.
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     area = models.IntegerField()
     pop2005 = models.IntegerField('Population 2005')
