6e2a0d3713c0c598935a354e5d596ba9d7af7b2f
Daniël Geerts <daniel.geerts@live.nl>
2021-08-24 16:36:20 +0200

    another LoRa API test

--- webapp/core/serializers/lorawan_serializer.py
+++ webapp/core/serializers/lorawan_serializer.py
@@ -23,32 +23,42 @@ class uplinkMessageSerializer(serializers.Serializer):
 class dataSerializer(serializers.Serializer):
     uplink_message = uplinkMessageSerializer(source="*")
 
+
+# Data field in the JSON
+class contextSerializer(serializers.Serializer):
+    class Meta:
+        fields = ['tentant-id']
+
 ###
 # Top layer of JSON
 ####
 class lorawanSerializer(serializers.HyperlinkedModelSerializer):
-    data = dataSerializer(source="*")
+    #data = dataSerializer(source="*")
+    context = contextSerializer(many=True,source="*")
 
     def create(self, validated_data):
-        rx_metadata = validated_data.pop('data').pop('uplink_message').pop('rx_metadata')
-        frm_payload = validated_data.pop('data').pop('uplink_message').pop('frm_payload')
+        #rx_metadata = validated_data.pop('data').pop('uplink_message').pop('rx_metadata')
+        #frm_payload = validated_data.pop('data').pop('uplink_message').pop('frm_payload')
         print("LORAWAN create: ", validated_data)
+        context = validated_data.pop('context')
 
-        for d in rx_metadata:
-            print(d, rx_metadata[d])
-        for d in frm_payload:
-            print(d, frm_payload[d])
+        for d in context:
+            print(d, context[d])
+        #for d in rx_metadata:
+            #print(d, rx_metadata[d])
+        #for d in frm_payload:
+            #print(d, frm_payload[d])
         for d in validated_data:
             print(d, validated_data[d])
 
         lora_obj = lorawanModel.objects.create(**validated_data)
-        lora_obj['geom'] = Point(frm_payload['lat'], frm_payload['lon'])
-        lora_obj['hdop'] = frm_payload['hdop']
-        lora_obj['alt'] = frm_payload['alt']
-        lora_obj['rssi'] = rx_metadata['rssi']
+        #lora_obj['geom'] = Point(frm_payload['lat'], frm_payload['lon'])
+        #lora_obj['hdop'] = frm_payload['hdop']
+        #lora_obj['alt'] = frm_payload['alt']
+        #lora_obj['rssi'] = rx_metadata['rssi']
 
         return lora_obj
 
     class Meta:
         model = lorawanModel
-        fields = ['received_at', 'data', 'geom', 'ack', 'msg', 'rssi', 'hdop', 'alt']
+        fields = ['received_at', 'context', 'geom', 'ack', 'msg', 'rssi', 'hdop', 'alt']
