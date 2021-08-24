c48ca1bf7456864115b9e0966a103f2fa4653f1b
Daniël Geerts <daniel.geerts@live.nl>
2021-08-24 16:23:35 +0200

    Possible fix for the LoRawan API

--- webapp/core/serializers/lorawan_serializer.py
+++ webapp/core/serializers/lorawan_serializer.py
@@ -3,19 +3,25 @@ from django.contrib.gis.geos import Point
 from ..models import lorawanModel
 
 # decoded-payload field in uplink_message in the JSON
-class decodedPayloadSerializer(serializers.Serializer):
+class frmPayloadSerializer(serializers.Serializer):
     class Meta:
         fields = ['alt', 'hdop', 'lat', 'lon']
 
 # Uplink_message field in data in the JSON
 class rxMetadataSerializer(serializers.Serializer):
     class Meta:
-        fields = ['rssi', '', 'lat', 'lon']
+        fields = ['rssi']
+
+# Data field in the JSON
+class uplinkMessageSerializer(serializers.Serializer):
+    rx_metadata = rxMetadataSerializer(many=True, source="*")
+    frm_payload = frmPayloadSerializer(source="*")
+    class Meta:
+        fields = ['rx_metadata', 'frm-payload']
 
 # Data field in the JSON
 class dataSerializer(serializers.Serializer):
-    rx_metadata = rxMetadataSerializer(many=True, source="*")
-    decoded_payload = decodedPayloadSerializer(source="*")
+    uplink_message = uplinkMessageSerializer(source="*")
 
 ###
 # Top layer of JSON
@@ -24,21 +30,21 @@ class lorawanSerializer(serializers.HyperlinkedModelSerializer):
     data = dataSerializer(source="*")
 
     def create(self, validated_data):
-        rx_metadata = validated_data.pop('data').pop('rx_metadata')
-        decoded_payload = validated_data.pop('data').pop('decoded_payload')
+        rx_metadata = validated_data.pop('data').pop('uplink_message').pop('rx_metadata')
+        frm_payload = validated_data.pop('data').pop('uplink_message').pop('frm_payload')
         print("LORAWAN create: ", validated_data)
 
         for d in rx_metadata:
             print(d, rx_metadata[d])
-        for d in decoded_payload:
-            print(d, decoded_payload[d])
+        for d in frm_payload:
+            print(d, frm_payload[d])
         for d in validated_data:
             print(d, validated_data[d])
 
         lora_obj = lorawanModel.objects.create(**validated_data)
-        lora_obj['geom'] = Point(decoded_payload['lat'], decoded_payload['lon'])
-        lora_obj['hdop'] = decoded_payload['hdop']
-        lora_obj['alt'] = decoded_payload['alt']
+        lora_obj['geom'] = Point(frm_payload['lat'], frm_payload['lon'])
+        lora_obj['hdop'] = frm_payload['hdop']
+        lora_obj['alt'] = frm_payload['alt']
         lora_obj['rssi'] = rx_metadata['rssi']
 
         return lora_obj
