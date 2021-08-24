c96b713bbabbc35f31c069e431300761f88e944e
Daniël Geerts <daniel.geerts@live.nl>
2021-08-24 17:14:58 +0200

    Will this work then? I hate nested JSON and Django API

--- webapp/core/models/__init__.py
+++ webapp/core/models/__init__.py
@@ -3,6 +3,6 @@ from .geo_messages import geoMessageModel, geoPointModel, geoCircleModel, geoPol
 from .ais_encoded_model import aisEncodedModel
 from .ais_decoded_model import aisDecodedModel
 from .dab_model import dabModel
-from .lorawan_model import *
+from .lorawan_model import lorawanModel
 
 from .fontawesome_model import FontAwesomeIcon
--- webapp/core/serializers/lorawan_serializer.py
+++ webapp/core/serializers/lorawan_serializer.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.contrib.gis.geos import Point
-from django.shortcuts import get_object_or_404
-from ..models import lorawanModel, ContextModel
+from ..models import lorawanModel
 
 # decoded-payload field in uplink_message in the JSON
 class frmPayloadSerializer(serializers.Serializer):
@@ -22,29 +21,19 @@ class uplinkMessageSerializer(serializers.Serializer):
 
 # Data field in the JSON
 class dataSerializer(serializers.Serializer):
-    uplink_message = uplinkMessageSerializer(source="*")
-
-
-# Data field in the JSON
-class contextSerializer(serializers.Serializer):
-    class Meta:
-        model = ContextModel
-        fields = '__all__'
+    received_at = serializers.DateTimeField()
+    ##uplink_message = uplinkMessageSerializer(source="*")
 
 ###
 # Top layer of JSON
 ####
 class lorawanSerializer(serializers.HyperlinkedModelSerializer):
-    #data = dataSerializer(read_only=True, source="*")
-    rssi = serializers.ReadOnlyField(source='data.uplink_message.rx_metadata[0].rssi')
-    lat = serializers.ReadOnlyField(source='data.uplink_message.decoded_payload.lat')
-    lon = serializers.ReadOnlyField(source='data.uplink_message.decoded_payload.lon')
-    alt = serializers.ReadOnlyField(source='data.uplink_message.decoded_payload.alt')
-    hdop = serializers.ReadOnlyField(source='data.uplink_message.decoded_payload.hdop')
+    data = dataSerializer()
 
     def create(self, validated_data):
         print("LORAWAN create: ", validated_data)
-
+        data = validated_data.pop('data')
+        print(data)
         lora_obj = lorawanModel.objects.create(**validated_data)
         return lora_obj
 
