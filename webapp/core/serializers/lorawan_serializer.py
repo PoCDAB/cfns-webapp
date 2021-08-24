9fbdb543cc2bcbe5d5b021aa48698c1cc90a3815
Daniël Geerts <daniel.geerts@live.nl>
2021-08-24 14:20:23 +0200

    Add more prints for LoRa API

--- webapp/core/serializers/lorawan_serializer.py
+++ webapp/core/serializers/lorawan_serializer.py
@@ -5,10 +5,14 @@ class lorawanSerializer(serializers.HyperlinkedModelSerializer):
 
     def validate(self, data):
         print("LORAWAN validate: ", data)
+        for d in data:
+            print(d, data[d])
         return data
 
     def create(self, validated_data):
         print("LORAWAN create: ", validated_data)
+        for d in validated_data:
+            print(d, validated_data[d])
         return lorawanModel.objects.create(**validated_data)
 
     class Meta:
