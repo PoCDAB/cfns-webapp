1ca6bedbfe1e9e1d5748a099dda36e2809f99c32
Daniël Geerts <daniel.geerts@live.nl>
2021-08-24 14:20:06 +0200

    Add base64 code to profile with password security

--- webapp/core/forms/__init__.py
+++ webapp/core/forms/__init__.py
@@ -2,3 +2,4 @@
 from .geoview_form import GeoviewForm
 from .send_dab_form import *
 from .contact_us import contactForm
+from .profile_authcode_form import viewBase64AuthcodeForm
--- webapp/core/views/profile_view.py
+++ webapp/core/views/profile_view.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from base64 import b64encode
+from ..forms import viewBase64AuthcodeForm
 
 @login_required(login_url='/login') #if not logged in redirect to /login
 def profileView(request):
-    return render(request, 'profile.html')
+    form = viewBase64AuthcodeForm(request.POST)
+    info_msg = None
+    info_type = None
+
+    if form.is_valid():
+        if request.user.check_password(request.POST['password']):
+            encodedUserAndPass = "{}:{}".format(request.user.username, request.POST['password']).encode("ascii")
+            userAndPass = b64encode(encodedUserAndPass).decode("ascii")
+            return render(request, 'profile.html', {'form': form, 'b64code': userAndPass})
+        else:
+            info_msg = 'The entered password does not match your current password...'
+            info_type = 'alert-danger'
+
+    return render(request, 'profile.html', {'form': viewBase64AuthcodeForm(), 'info_msg': info_msg, 'info_type': info_type})
