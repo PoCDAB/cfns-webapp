f525382c069c73dc8cbed8888bbf49b0ebb398c1
Daniël Geerts <daniel.geerts@live.nl>
2021-08-25 16:09:58 +0200

    Reverted some files from development branch

--- webapp/core/models/lorawan_model.py
+++ webapp/core/models/lorawan_model.py
@@ -3,31 +3,13 @@ from django.contrib.gis.db import models as gismodels
 from django.db.models import Manager as GeoManager
 from .base_model import BaseModel
 
-
-class DecodedPayloadModel(gismodels.Model):
-    alt = models.IntegerField()
-    hdop = models.DecimalField(max_digits=19, decimal_places=2)
-    lat = models.DecimalField(max_digits=19, decimal_places=2)
-    lon = models.DecimalField(max_digits=19, decimal_places=2)
-
-class UplinkMessageModel(gismodels.Model):
-    session_key_id = models.CharField(max_length=256)
-    f_port = models.IntegerField(blank=True, null=True)
-    f_cnt = models.IntegerField(blank=True, null=True)
-    frm_payload = models.CharField(max_length=256)
-
-    decoded_payload = models.ForeignKey(DecodedPayloadModel, on_delete=models.CASCADE, blank=True, null=True)
-
-class DataModel(gismodels.Model):
-    type = models.CharField(max_length=256)
-    received_at = models.DateTimeField(blank=True, null=True)
-
-    uplink_message = models.ForeignKey(UplinkMessageModel, on_delete=models.CASCADE, blank=True, null=True)
+class gatewayModel(BaseModel):
+    rssi = models.IntegerField(blank=True, null=True)
+    snr = models.IntegerField(blank=True, null=True)
+    gateway_id = models.CharField(blank=True, null=True, max_length=256,)
+    gateway_eui = models.CharField(blank=True, null=True, max_length=256,)
 
 class lorawanModel(BaseModel):
-    #geom = gismodels.PointField('Location', null=True, blank=True,)
-    #objects = GeoManager()
-
     ack = models.IntegerField('Acknowledgement',
         blank=True,
         null=True
@@ -37,13 +19,13 @@ class lorawanModel(BaseModel):
         null=True,
         max_length=256,
     )
-    rssi = models.IntegerField(blank=True, null=True)
     hdop = models.DecimalField(blank=True, null=True, max_digits=19, decimal_places=2)
     alt = models.DecimalField(blank=True, null=True, max_digits=19, decimal_places=2)
 
-    received_at = models.DateTimeField(blank=True, null=True)
+    gateways = models.ManyToManyField(gatewayModel)
 
-    #data = models.ForeignKey(DataModel, on_delete=models.CASCADE, blank=True, null=True)
+    geom = gismodels.PointField('Location', null=True, blank=True,)
+    objects = GeoManager()
 
     class Meta:
         verbose_name = 'LoRaWAN message'
--- webapp/core/serializers/lorawan_serializer.py
+++ webapp/core/serializers/lorawan_serializer.py
@@ -1,28 +1,6 @@
 from rest_framework import serializers
 from django.contrib.gis.geos import Point
-from ..models import lorawanModel, DataModel, UplinkMessageModel, DecodedPayloadModel
-
-# decoded-payload field in uplink_message in the JSON
-class DecodedPayloadSerializer(serializers.Serializer):
-    class Meta:
-        model = DecodedPayloadModel
-        fields = ['alt', 'hdop', 'lat', 'lon']
-
-# Data field in the JSON
-class uplinkMessageSerializer(serializers.Serializer):
-    DecodedPayloadModel = DecodedPayloadSerializer(source="*")
-
-    class Meta:
-        model = UplinkMessageModel
-        fields = '__all__'
-
-# Data field in the JSON
-class dataSerializer(serializers.Serializer):
-    #uplink_message = uplinkMessageSerializer(source="*")
-
-    class Meta:
-        model = DataModel
-        fields = ['type', 'received_at',]
+from ..models import lorawanModel, gatewayModel
 
 ###
 # Top layer of JSON
