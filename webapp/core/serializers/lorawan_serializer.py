7706a21805fead0f8be852df35e35a2b00c90f85
Daniël Geerts <daniel.geerts@live.nl>
2021-08-25 12:12:40 +0200

    Okay

--- webapp/core/serializers/lorawan_serializer.py
+++ webapp/core/serializers/lorawan_serializer.py
@@ -32,12 +32,7 @@ class lorawanSerializer(serializers.HyperlinkedModelSerializer):
         print("=== self ===")
         print(self)
         print("=== context ===")
-        print(self.context["request"].data)
-        print("=== data 1 ===")
-        print(self.data)
-        print("=== data 2 ===")
-        for d in self.data:
-            print(d)
+        print(self.context["request"].data["uplink_message"])
         print("=================== DONE ===========================")
 
         lora_obj = lorawanModel.objects.create(**validated_data)
