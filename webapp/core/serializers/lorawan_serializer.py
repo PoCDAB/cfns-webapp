a388e81312a34b8976f1f2c82a5903a05d39fd3b
Daniël Geerts <daniel.geerts@live.nl>
2021-08-25 14:36:19 +0200

    Fixed lorawan_serializer

--- webapp/core/serializers/lorawan_serializer.py
+++ webapp/core/serializers/lorawan_serializer.py
@@ -10,25 +10,33 @@ class lorawanSerializer(serializers.HyperlinkedModelSerializer):
     def create(self, validated_data):
         allJSONData = self.context["request"].data
         lora_obj = lorawanModel.objects.create(**validated_data)
-        noneObj = allJSONData["uplink_message"]["decoded_payload"]["ikbestajanietmienjong"]
-        print(noneObj)
-        lat = allJSONData["uplink_message"]["decoded_payload"]["lat"]
-        lon = allJSONData["uplink_message"]["decoded_payload"]["lon"]
 
-        lora_obj.geom = Point(lat, lon)
-        lora_obj.alt = allJSONData["uplink_message"]["decoded_payload"]["alt"]
-        lora_obj.hdop = allJSONData["uplink_message"]["decoded_payload"]["hdop"]
-        lora_obj.alt = allJSONData["uplink_message"]["decoded_payload"]["alt"]
-        for gateway in allJSONData["uplink_message"]["rx_metadata"]:
-            gateway_obj = gatewayModel.objects.create()
-            gateway_obj.rssi = gateway["rssi"]
-            gateway_obj.snr = gateway["snr"]
-            gateway_obj.gateway_id = gateway["gateway_id"]
-            gateway_obj.gateway_eui = gateway["gateway_eui"]
-            gateway_obj.save()
-            lora_obj.gateways.add(gateway_obj)
+        decoded_payload = allJSONData["uplink_message"]["decoded_payload"]
+        decoded_payload_keys = ('lat', 'lon', 'alt', 'hdop', 'alt')
+        if decoded_payload and set(decoded_payload_keys).issubset(decoded_payload):
+            lat = decoded_payload["lat"]
+            lon = decoded_payload["lon"]
 
-        lora_obj.save()
+            lora_obj.geom = Point(lat, lon)
+            lora_obj.alt = decoded_payload["alt"]
+            lora_obj.hdop = decoded_payload["hdop"]
+            lora_obj.alt = decoded_payload["alt"]
+            rx_metadata =  allJSONData["uplink_message"]["rx_metadata"]
+            if rx_metadata:
+                for gateway in rx_metadata:
+                    gateway_keys = ('rssi', 'snr', 'gateway_id', 'gateway_eui')
+                    if set(gateway_keys).issubset(gateway):
+                        gateway_obj = gatewayModel.objects.create()
+                        gateway_obj.rssi = gateway["rssi"]
+                        gateway_obj.snr = gateway["snr"]
+                        gateway_obj.gateway_id = gateway["gateway_id"]
+                        gateway_obj.gateway_eui = gateway["gateway_eui"]
+                        gateway_obj.save()
+                        lora_obj.gateways.add(gateway_obj)
+            lora_obj.save()
+        else:
+            print("Something went wrong with the lorawanSerializer")
+            return None
         return lora_obj
 
     class Meta:
