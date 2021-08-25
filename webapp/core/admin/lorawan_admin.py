5f77f8801af620a27aef8448dbdd02362aa4e057
Daniël Geerts <daniel.geerts@live.nl>
2021-08-25 12:56:29 +0200

    WINNER WINNER CHICKENDINNER !!!

--- webapp/core/admin/lorawan_admin.py
+++ webapp/core/admin/lorawan_admin.py
@@ -2,17 +2,18 @@ from django.contrib import admin
 from ..models import dabModel
 
 class lorawanAdmin(admin.ModelAdmin):
-    list_display = ('id', 'ack', 'msg', 'rssi', 'hdop', 'alt')
+    list_display = ('id', 'ack', 'msg', 'hdop', 'alt', 'geom')
     list_filter = ()
 
     fieldsets = [
         ("BaseModel", {'fields': ['id', 'created_at', 'updated_at']}),
-        ("LoRaWAN Model", {'fields': ['received_at', 'ack', 'msg', 'rssi', 'hdop', 'alt', 'geom']}),
+        ("LoRaWAN Model", {'fields': ['ack', 'msg', 'hdop', 'alt', 'geom']}),
+        ("Gateways", {'fields': ['gateways']}),
     ]
 
-    search_fields = ('id', 'ack', 'msg', 'rssi', 'hdop', 'alt')
-    ordering = ('id', 'ack', 'msg', 'rssi', 'hdop', 'alt', 'created_at', 'updated_at',)
-    readonly_fields = ('id', 'created_at', 'updated_at', 'received_at', )
+    search_fields = ('id', 'ack', 'msg', 'hdop', 'alt', 'gateways')
+    ordering = ('id', 'ack', 'msg', 'hdop', 'alt', 'created_at', 'updated_at',)
+    readonly_fields = ('id', 'created_at', 'updated_at', 'gateways')
     filter_horizontal = ()
 
     class Meta:
--- webapp/core/serializers/lorawan_serializer.py
+++ webapp/core/serializers/lorawan_serializer.py
@@ -1,46 +1,36 @@
 from rest_framework import serializers
 from django.contrib.gis.geos import Point
-from ..models import lorawanModel, DataModel, UplinkMessageModel, DecodedPayloadModel
-
-# decoded-payload field in uplink_message in the JSON
-class DecodedPayloadSerializer(serializers.Serializer):
-    class Meta:
-        model = DecodedPayloadModel
-        fields = ['alt', 'hdop', 'lat', 'lon']
-
-# Data field in the JSON
-class uplinkMessageSerializer(serializers.Serializer):
-    DecodedPayloadModel = DecodedPayloadSerializer(source="*")
-
-    class Meta:
-        model = UplinkMessageModel
-        fields = '__all__'
-
-# Data field in the JSON
-class dataSerializer(serializers.Serializer):
-    #uplink_message = uplinkMessageSerializer(source="*")
-
-    class Meta:
-        model = DataModel
-        fields = ['type', 'received_at',]
+from ..models import lorawanModel, gatewayModel
 
 ###
-# Top layer of JSON
+# LoRaWAN serializer
+# POST data from TheThingsNetwork (TTN) is parsed to the 
 ####
 class lorawanSerializer(serializers.HyperlinkedModelSerializer):
     def create(self, validated_data):
         allJSONData = self.context["request"].data
-
-        print("=== context ===")
-        print('alt', allJSONData["uplink_message"]["decoded_payload"]["alt"])
-        print('hdop', allJSONData["uplink_message"]["decoded_payload"]["hdop"])
-        print('lat', allJSONData["uplink_message"]["decoded_payload"]["lat"])
-        print('lon', allJSONData["uplink_message"]["decoded_payload"]["lon"])
-        print("=================== DONE ===========================")
-
         lora_obj = lorawanModel.objects.create(**validated_data)
+        noneObj = allJSONData["uplink_message"]["decoded_payload"]["ikbestajanietmienjong"]
+        print(noneObj)
+        lat = allJSONData["uplink_message"]["decoded_payload"]["lat"]
+        lon = allJSONData["uplink_message"]["decoded_payload"]["lon"]
+
+        lora_obj.geom = Point(lat, lon)
+        lora_obj.alt = allJSONData["uplink_message"]["decoded_payload"]["alt"]
+        lora_obj.hdop = allJSONData["uplink_message"]["decoded_payload"]["hdop"]
+        lora_obj.alt = allJSONData["uplink_message"]["decoded_payload"]["alt"]
+        for gateway in allJSONData["uplink_message"]["rx_metadata"]:
+            gateway_obj = gatewayModel.objects.create()
+            gateway_obj.rssi = gateway["rssi"]
+            gateway_obj.snr = gateway["snr"]
+            gateway_obj.gateway_id = gateway["gateway_id"]
+            gateway_obj.gateway_eui = gateway["gateway_eui"]
+            gateway_obj.save()
+            lora_obj.gateways.add(gateway_obj)
+
+        lora_obj.save()
         return lora_obj
 
     class Meta:
         model = lorawanModel
-        fields = ['id', 'created_at', 'updated_at', 'ack', 'msg', 'rssi', 'hdop', 'alt', 'received_at']
+        fields = ['id', 'created_at', 'updated_at', 'geom', 'ack', 'msg', 'hdop', 'alt', 'gateways']
